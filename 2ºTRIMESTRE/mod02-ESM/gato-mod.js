c422b1ccee61adbdbe066672ecc7b2f3e8af0b7c
heloisakalinowski-cpu <heloisa.kalinowski@escola.pr.gov.br>
2026-05-27 10:51:07 +0000

    finalização módulo ESM - petShop

---
+++ 2ºTRIMESTRE/mod02-ESM/gato-mod.js
@@ -0,0 +1,15 @@
+//USA PADRÃO MODERNO IMPORT/EXPORT - EMS
+//aqui criaremos nossos módulos
+
+export function BanhoGato(raca, tipobanho)
+{
+    return 'O gato da raça ${raca} tomou um banho de ${tipobanho}'
+}
+export function AlimentarGato (raca, tipocomida)
+{
+    return 'O gato da raça  ${raca} foi alimentado com ${tipocomida}'
+}
+export function TosaGato (raca, tipotosa)
+{
+    return 'O gato da raça  ${raca} o gato foi tosado ${tipotosa}'
+}
---
+++ 2ºTRIMESTRE/mod02-ESM/main-apelido-mod.js
@@ -0,0 +1,4 @@
+// Usamos o * para dizer que estamos 
+// importando TUDO de um arquivo
+// o termo "as" permite criar um apelido
+
