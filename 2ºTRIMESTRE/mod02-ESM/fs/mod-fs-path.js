a4cf3b0feca4e6e030681cb18db28e5a5108b4c3
heloisakalinowski-cpu <heloisa.kalinowski@escola.pr.gov.br>
2026-06-17 11:40:15 +0000

    finalizado tratamento de erros com o Chalk

--- 2ºTRIMESTRE/mod02-ESM/fs/mod-fs-path.js
+++ 2ºTRIMESTRE/mod02-ESM/fs/mod-fs-path.js
@@ -1,12 +1,13 @@
-const fs = require('fs')
+const fs = require('fs');
 const path = require('path')
 const chalk = require('chalk').default
 
 try {
-    const arquivo = fs.readaFileySync(path.join(__dirname,'/festa.md'), 'utf-8');
-    console.log(chalk.green('Arquivo carregado com sucesso'))
+    const arquivo = fs.readFileySync(path.join(__dirname, '/festa.md'), 'utf-8');
+    console.log(chalk.green("🎉 Arquivo carregado com sucesso!"))
     console.log(arquivo);
 } cacth (erro) {
-    console.log(chalk.red("Não deu certo:"))
+    console.log(chalk.bgYellowBright.red('☢️ Atenção, erro a frente'))
+    console.log(chalk.red("🛑 Deu errado:"))
     console.log(erro.message)
 }
