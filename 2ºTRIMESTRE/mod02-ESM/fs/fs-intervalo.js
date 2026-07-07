7698177e57b921536fd8dc6ba902a05a8054fdab
heloisakalinowski-cpu <heloisa.kalinowski@escola.pr.gov.br>
2026-07-07 11:44:54 +0000

    fizemos algo

---
+++ 2ºTRIMESTRE/mod02-ESM/fs/fs-intervalo.js
@@ -0,0 +1,10 @@
+// setTimeout(ms) - executa 1 vez
+setTimeout(function(){
+    console.log('Executou timeout...')
+}, 3000)
+
+
+//setInterval(ms) - executa 'x' vezes
+setInterval(function(){
+    console.log('Executou FPS 30')
+}, 2000)
