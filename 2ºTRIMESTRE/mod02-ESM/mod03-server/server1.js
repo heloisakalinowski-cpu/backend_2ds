d58151f515c06c1d2b08106588dc7f3787c843e2
heloisakalinowski-cpu <heloisa.kalinowski@escola.pr.gov.br>
2026-08-05 11:46:48 +0000

    continuação

--- 2ºTRIMESTRE/mod02-ESM/mod03-server/server1.js
+++ 2ºTRIMESTRE/mod02-ESM/mod03-server/server1.js
@@ -1,16 +1,31 @@
 const server = require('node:http')
+const path = require('node:path')
+const fs = require('node:fs')
 const porta = 8005
+// Carrega o caminho dos arquivos
+const home = path.join(__dirname, 'pages/index.html')
+const home = path.join(__dirname, 'pages/sobre.html')
 
-const server = http.createServer((req,res) =>{
-    //qual informação eu tenho?
+const server = http.createServer((req, res) => {
+    // qual informação eu tenho?
     const urlTratada = new URL(req.url, 'http://$(req.headers.headers.host)')//limpeza da url
     const recurso = urlTratada.pathname // em qual rota estamos?
 
-    if(recurso === '/'){
-res.setHeader('Content-Type', 'text/html; charset=utf-8')
-res.statusCode = 200
-res.end('<h3>Página Incial</h3>')
+    if(recurso === '/'){ //Página inicial
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    return res.statusCode = 200
+    res.end(fs.readFileSync(home, 'utf-8'))
+
+    } if(recurso === '/sobre'){ // Sobre nós
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    return res.statusCode = 200
+    res.end(fs.readFileSync(home, 'utf-8'))
     } else {
-    res.setHeader('Content-Type','text/html;')
+    res.statusCode = 401
+    res.setHeader('Content-Type','text/html; charset=utf-8')
+    res.end('401 Não Autorizado!')
     }
+})
+server.listen(porta, () ==> {
+    console.log('Servidor rodando na porta ${porta}')
 })
