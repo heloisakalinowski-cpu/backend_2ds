0a75351331cc127f0e5005aa5af06484ca9f3f2c
heloisakalinowski-cpu <heloisa.kalinowski@escola.pr.gov.br>
2026-08-12 11:48:14 +0000

    continuação

--- 2ºTRIMESTRE/mod02-ESM/mod03-server/server1.js
+++ 2ºTRIMESTRE/mod02-ESM/mod03-server/server1.js
@@ -1,31 +1,28 @@
-const server = require('node:http')
+const http = require('node:http')
 const path = require('node:path')
 const fs = require('node:fs')
 const porta = 8005
-// Carrega o caminho dos arquivos
 const home = path.join(__dirname, 'pages/index.html')
-const home = path.join(__dirname, 'pages/sobre.html')
+const sobre = path.join(__dirname, 'pages/sobre.html')
 
 const server = http.createServer((req, res) => {
-    // qual informação eu tenho?
-    const urlTratada = new URL(req.url, 'http://$(req.headers.headers.host)')//limpeza da url
-    const recurso = urlTratada.pathname // em qual rota estamos?
+ const urlTratada = new URL(req.url, `http://${req.headers.host}`)
+ const recurso = urlTratada.pathname 
 
-    if(recurso === '/'){ //Página inicial
+ if (recurso === '/') {
+    res .statusCode = 200
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    return res.statusCode = 200
-    res.end(fs.readFileSync(home, 'utf-8'))
+    return res.end(fs.readFileSync(home, 'utf-8'))
+} else if (recurso === '/sobre') {
+    res .statusCode = 200
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    return res.end(fs.readFileSync(sobre, 'utf-8'))
+} else {
+    res .statusCode = 401
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    return res.end('<h3>Erro 401 - Não autorizado</h3>')
+}})
 
-    } if(recurso === '/sobre'){ // Sobre nós
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    return res.statusCode = 200
-    res.end(fs.readFileSync(home, 'utf-8'))
-    } else {
-    res.statusCode = 401
-    res.setHeader('Content-Type','text/html; charset=utf-8')
-    res.end('401 Não Autorizado!')
-    }
-})
-server.listen(porta, () ==> {
-    console.log('Servidor rodando na porta ${porta}')
+server.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`)
 })
